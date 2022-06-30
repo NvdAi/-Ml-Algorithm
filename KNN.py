3feb76d303004abb368705966573d72cb23e537d
Nvd <nvdnzai@gmail.com>
2022-06-30 02:32:44 -0400

    commited

---
+++ KNN.py
@@ -0,0 +1,65 @@
+
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_blobs
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import numpy as np 
+import collections
+import random
+import statistics as st
+
+class Knn:
+    def __init__(self, n_neighbors):
+        self.n_neighbors = n_neighbors
+
+    def fit(self, X, Y):
+        self.X = X
+        self.Y = Y
+        return self.X , self.Y
+
+    def predict(self, x):
+        predicted_labels = []
+        for point in x:
+            dis = list(np.sqrt(np.sum((self.X - point)**2, axis=1) ))
+            dis_sorted = sorted(dis)
+            min_distances = dis_sorted[0:self.n_neighbors]
+            LNP = []
+            for i in min_distances:
+                inx = dis.index(i)
+                label_of_each_near_point = int(self.Y[inx])
+                LNP.append(label_of_each_near_point)
+            frequency = collections.Counter(LNP)
+            keys = list(frequency.keys())
+            vals = list(frequency.values())
+            m_freq = max(frequency.values())
+            position = vals.index(m_freq)
+            pred_label = keys[position]
+            predicted_labels.append(pred_label)
+            LNP = []
+        return predicted_labels
+
+
+if __name__ == "__main__":
+    DATA = ()
+    data_list  = ('X','Y' , 'x', 'y')
+    for i in data_list:
+        TEMP = np.loadtxt(i+'.txt')
+        DATA = DATA+(TEMP,)
+    train_data,train_labels, test_data,  test_labels = DATA
+    k_near_Neighbors = Knn(n_neighbors = 7)  
+    k_near_Neighbors.fit(train_data, train_labels)
+    predictions = k_near_Neighbors.predict(test_data,K=7)
+    accuracy = accuracy_score(predictions, test_labels)
+    print("accuracy : ",accuracy)
+
+    # colours = ('green', 'red', 'blue')
+    # n_classes = 3
+    # for n_class in range(0, n_classes):
+    #     plt.scatter(train_data[train_labels==n_class, 0], train_data[train_labels==n_class, 1], c=colours[n_class], s=20, label=str(n_class))
+    #     plt.scatter(test_data[test_labels==n_class, 0], test_data[test_labels==n_class, 1], c=colours[n_class], s=80)
+    # plt.legend(loc='upper right');
+    # plt.show()
+
+
+
+
---
+++ main_code.py
@@ -0,0 +1,62 @@
+from sklearn.datasets import make_blobs
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+from KNN import Knn
+
+
+centers = [[2, 3], [5, 5], [1, 8]]
+n_classes = len(centers)
+data, labels = make_blobs(n_samples=300,  centers=np.array(centers),random_state=None,cluster_std=2)
+res = train_test_split(data, labels, train_size=0.8, test_size=0.2, random_state=None)
+train_data, test_data, train_labels, test_labels = res 
+# print(train_data,  train_labels,test_data, test_labels)
+
+k = 7
+# scikit leartn class
+scikitknn = KNeighborsClassifier(n_neighbors=k)
+# my class
+nvdknn = Knn(n_neighbors=k)
+
+# compare two object
+knnList = [scikitknn, nvdknn]
+knnList_name = ["scikitknn", "nvdknn"]
+acc_list = []
+for i,knn in enumerate(knnList):
+    knn.fit(train_data, train_labels) 
+    predicted = knn.predict(test_data)
+    acc = accuracy_score(predicted, test_labels)
+    acc_list.append(acc)
+    print(" the best accuracy_score of",knnList_name[i]+' =', acc)
+
+# visya
+
+plt.figure("KNN ALGORITHM")
+plt.subplot(1,2,1)
+colours = ('green', 'red', 'blue')
+n_classes = 3
+for n_class in range(0, n_classes):
+    plt.scatter(train_data[train_labels==n_class, 0], train_data[train_labels==n_class, 1], c=colours[n_class], s=20, label=str(n_class))
+    plt.scatter(test_data[test_labels==n_class, 0], test_data[test_labels==n_class, 1], c=colours[n_class], s=80)
+plt.legend(loc='upper right');
+
+plt.subplot(1,2,2)
+plt.scatter(k, acc_list[0], c="b", s=50, label=knnList_name[1])
+plt.scatter(k+1, acc_list[0], c="r", s=50, label=knnList_name[0])
+plt.xlabel('K-Nearest Neighbors',fontsize=15)
+plt.ylabel('Accuracy',fontsize=15)
+plt.xlim(5, k+3)
+plt.xticks(np.arange(k,k+2), ['7', '7'])
+plt.legend(loc='upper right')
+plt.show()
+
+# git clone
+# git pull
+# git push 
+# git add .
+# git commit 
+# git branch 
+
