2ebbc4ee68f318733b1f73079315d8c9efa4469c
Nvd <nvdnzai@gmail.com>
2022-08-15 17:53:34 -0400

    update

--- MLR_pyTORCH/code/mlr3d.torch.py
+++ MLR_pyTORCH/code/mlr3d.torch.py
@@ -1,5 +1,8 @@
 import torch
 import csv
+import os
+import cv2 
+from PIL import Image 
 import numpy as np
 import pandas as pd
 from torch.autograd import Variable
@@ -19,19 +22,43 @@ class LinearRegressionModel(torch.nn.Module):
 		y_pred = self.linear(x)
 		return y_pred
 
-def visualize(point_lists,xp,y_data):
+def generate_video():
+	image_folder = '../data/figures/' 
+	outpot_path = '../data/outpot'
+	os.makedirs(outpot_path, exist_ok=True)
+	video_name = os.path.join(outpot_path, " my_moldel_outpot.avi")
+	images = [img for img in os.listdir(image_folder)
+			  if img.endswith(".jpg") or
+				 img.endswith(".jpeg") or
+				 img.endswith("png")]
+	images = sorted(images,key=lambda fname: int(fname.split('.')[0]))
+	frame = cv2.imread(os.path.join(image_folder, images[0]))
+	height, width, layers = frame.shape  
+	video = cv2.VideoWriter(video_name, 0, 1, (width, height)) 
+	for image in images: 
+		video.write(cv2.imread(os.path.join(image_folder, image))) 
+	cv2.destroyAllWindows() 
+	video.release()  
+
+def visualize(point_lists,xp,y_data,i,epo,n_epoch):
 	ax = plt.axes(projection ='3d')
-	for i in range(len(point_lists[0])):
-		if i==len(point_lists[0])-1:
-			ax.plot_surface(point_lists[0][i], point_lists[1][i], point_lists[2][i], alpha=0.5, color='b')
-		else:
-			ax.plot_surface(point_lists[0][i], point_lists[1][i], point_lists[2][i], alpha=0.2, color='y')
+	color,alpha = ("y",0.2)
+	if epo==n_epoch-1:
+		color,alpha = ("b",0.5)
+	ax.plot_surface(point_lists[0][0], point_lists[1][0], point_lists[2][0], alpha=alpha, color=color)
 	ax.scatter3D(xp[:,0], xp[:,1],y_data,c="r")
+	ax.set_xlim([0, 85])
+	ax.set_ylim([0, 35])
+	ax.set_zlim([0, 30])
 	ax.set_title("This dataset have two independed features (x1,x2)\nand one depended variable (Y)")
 	ax.set_xlabel('X1',fontsize=20,labelpad=12)
 	ax.set_ylabel('X2',fontsize=20,labelpad=12)
-	ax.set_zlabel('Y',fontsize=20,labelpad=12)
-
+	ax.set_zlabel('Y',fontsize=20, labelpad=12)
+	save_frames_path = "../data/figures"
+	os.makedirs(save_frames_path, exist_ok=True)
+	ax.figure.savefig(save_frames_path + "/" + str(i) +  ".png")
+	print("sived figure :",i)
+			
 def drow_surface(tetha_0, tetha_12, point_lists):
 	xx, yy = np.meshgrid(range(-5,80), range(-5,35))
 	z = (-tetha_12[0] * xx - tetha_12[1] * yy - tetha_0) * 1. /(-1)
@@ -56,17 +83,16 @@ xp = x_data # >> for plot;beacuse x_data must be shuffle
 # define parameters
 input_size, output_size = (x_data.shape[1],y_data.shape[1])
 learning_rate = 0.01
-n_epoch = 200
+n_epoch = 400
 batch_size = 25
-EIO = 50 # Every 100 itreation once plot
+EIO = 10             # Every itreation once plot
 iteration = int(x_data.shape[0]/batch_size)
 
 #define model
 our_model = LinearRegressionModel()
 criterion = torch.nn.MSELoss(size_average = True)
 optimizer = torch.optim.Adam(our_model.parameters(), lr = learning_rate)
-
-point_lists = [[],[],[]]
+i = 0
 for epoch in range(n_epoch):
 	start,stop = (0,0)
 	for iteer in range(iteration):
@@ -79,18 +105,21 @@ for epoch in range(n_epoch):
 		optimizer.step()
 		# print('epoch {}, iteration {}, loss {}'.format(epoch, iteer, loss.item()))
 	x_data = x_data[torch.randperm(x_data.size()[0])]  # shuffle data
-	# print("=============================================")
 	if epoch %EIO==0 or epoch==n_epoch-1:
-		print("epoc:",epoch)	
+		# print("=============================================")
+		point_lists = [[],[],[]]
 		tetha_0 = our_model.linear.bias.tolist()[0]
 		tetha_12 = our_model.linear.weight.tolist()[0]
-		print("Intereptc:",tetha_0,"\nCoefficients(tetha_1....tetha2):",tetha_12)
+		# print("Intereptc:",tetha_0,"\nCoefficients(tetha_1....tetha2):",tetha_12)
 		drow_surface(tetha_0, tetha_12, point_lists)
-	# print("=============================================")
+		print("=============================================")
+		visualize(point_lists,xp,y_data,i,epoch,n_epoch)
+		i+=1
 
-visualize(point_lists,xp,y_data)
-
-plt.show()
+print("=================== Model trained ========================")
+print("=================== ALL figures saved ========================")
+# plt.show()
+generate_video()
 '''
 Intereptc: 15.01642374536851 
 Coefficients(theta_1 ... theta_n): [-0.20059503  0.17741778]
